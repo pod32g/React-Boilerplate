b4d10dd8f54bc5315766f3ba0c20cd1af6666ff7
pod32g <david_ivan@outlook.com>
2020-07-20 14:12:13 -0500

    First commit

---
+++ src/Routes/MainRouter.tsx
@@ -0,0 +1,10 @@
+import { MainView } from '../Views';
+
+const MainRouter = [
+    {
+        path: "/",
+        component: MainView
+    }
+]
+
+export default MainRouter;
---
+++ src/Views/MainView/MainView.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import './MainView.style.scss'
+
+export const MainView: FunctionComponent = () => {
+    return (
+        <h1>Hello Banana</h1>
+    )
+}
